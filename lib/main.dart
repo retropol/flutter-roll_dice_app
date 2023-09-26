7c21e7bc56b2580f246c20db4f18107d2973b71c
Mehmet Demirtürk <92218659+retropol@users.noreply.github.com>
2023-09-26 16:26:55 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ void main() {
         decoration: const BoxDecoration(
             gradient: LinearGradient(
           colors: [
-            Color.fromARGB(255, 255, 183, 173),
+            Color.fromARGB(255, 255, 160, 148),
             Color.fromARGB(255, 53, 88, 247)
           ],
         )),
         child: const Center(
-          child: Text("Hello World!"),
+          child: Text("Hello World!!"),
         ),
       ),
     ),
